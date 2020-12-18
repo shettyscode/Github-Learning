0c234103ae45e0deef014c3533330d132dd66881
shettyscode <76159171+shettyscode@users.noreply.github.com>
2020-12-18 15:51:15 +0530

    Update FirstTest.java

--- Git Demo/src/com/cjc/FirstTest.java
+++ Git Demo/src/com/cjc/FirstTest.java
@@ -4,9 +4,13 @@ public class FirstTest {
 
 	public static void main(String[] args) {
 		System.out.println("Why so Serious.....!");
--	
+	
 	}
 public void m1() {
 	System.out.println("m1 method");
 	}
+	public void m2() {
+	System.out.println("m2 method");
+	}
+	
 }
